279f8bf6b62c794ea23b9bd2b76a0cd9c1366657
Karthick11dec <73515462+Karthick11dec@users.noreply.github.com>
2021-06-30 20:20:43 +0530

    new feature

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ const mongoClient = mongodb.MongoClient;
 const objectId = mongodb.ObjectID;
 // const DB_URL = "mongodb://127.0.0.1:27017";
 const DB_URL = process.env.DBURL || "mongodb://127.0.0.1:27017";
+const database = "secretMessage";
+const Docs = "data";
+
 const PORT = process.env.PORT || 5000;
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
-const saltrounds = 10;
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: EMAIL,
-        pass: PASSWORD,
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
     }
 })
 
 router.get('/', async (req, res) => {
-    res.send("secret message service")
+    res.send("secret message service");
 })
 
 router.post('/create-message', async (req, res) => {
     try {
         const client = await mongoClient.connect(DB_URL);
-        const db = client.db('secretMessage');
-        const salt = await bcrypt.genSalt(saltrounds);
+        const db = client.db(database);
+        const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
         const data = {
             password: hash,
@@ -44,17 +44,16 @@ router.post('/create-message', async (req, res) => {
             message: req.body.message,
             mail: req.body.targetMail
         }
-        await db.collection('secretMessage').insertOne(data);
-        const result = await db.collection('secretMessage').findOne({ key: data.key });
+        await db.collection(Docs).insertOne(data);
+        const result = await db.collection(Docs).findOne({ key: data.key });
 
         //for receiver
-        const usrMailUrl = `${req.body.targetURL}?rs=${result._id}`;
         await transporter.sendMail({
             from: process.env.EMAIL,
             to: req.body.targetMail,
             subject: "S*CR*T M*SSAG*",
             html: `<p>Hi this is ${process.env.EMAIL},<br /><br />
-        I have a SECRET MESSAGE for you <a href='${usrMailUrl}' target='_blank'>Click here.</a><br />
+        I have a SECRET MESSAGE for you <a href='${req.body.targetURL}?rs=${result._id}' target='_blank'>Click here.</a><br />
         <p>Note : Don't share with Anyone...</p>
      </p>`
         });
@@ -89,8 +88,8 @@ router.post('/create-message', async (req, res) => {
 router.get('/message-by-id/:id', async (req, res) => {
     try {
         const client = await mongoClient.connect(DB_URL);
-        const db = client.db('secretMessage');
-        const result = await db.collection('secretMessage')
+        const db = client.db(database);
+        const result = await db.collection(Docs)
             .find({ _id: objectId(req.params.id) })
             .project({ password: 0, _id: 0, key: 0 }).toArray();
         res.status(200).json({ message: "message have been fetched successfully.", result });
@@ -104,12 +103,12 @@ router.get('/message-by-id/:id', async (req, res) => {
 router.delete('/delete-message', async (req, res) => {
     try {
         const client = await mongoClient.connect(DB_URL);
-        const db = client.db('secretMessage');
-        const secret = await db.collection('secretMessage').findOne({ key: req.body.secretKey });
+        const db = client.db(database);
+        const secret = await db.collection(Docs).findOne({ key: req.body.secretKey });
         if (secret) {
             const compare = await bcrypt.compare(req.body.password, secret.password);
             if (compare) {
-                await db.collection('secretMessage').findOneAndDelete({ key: req.body.secretKey });
+                await db.collection(Docs).findOneAndDelete({ key: req.body.secretKey });
                 res.status(200).json({ message: `${secret.mail}'s - message has been deleted successfully.` });
             } else {
                 res.status(401).json({ message: "incorrect password!" });

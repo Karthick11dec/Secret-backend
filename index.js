b8785906221c7bc6cd2aa8a68386c4da3072516c
Karthick11dec <73515462+Karthick11dec@users.noreply.github.com>
2021-06-30 18:57:18 +0530

    updation

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 const mongoClient = mongodb.MongoClient;
 const objectId = mongodb.ObjectID;
-const DB_URL = "mongodb://127.0.0.1:27017";
-// const DB_URL = process.env.DBURL || "mongodb://127.0.0.1:27017";
+// const DB_URL = "mongodb://127.0.0.1:27017";
+const DB_URL = process.env.DBURL || "mongodb://127.0.0.1:27017";
 const PORT = process.env.PORT || 5000;
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
@@ -76,12 +76,11 @@ router.post('/create-message', async (req, res) => {
 
         await transporter.sendMail(mailData);
 
-        res.json({ message: "Ack of this message has been sent to your mail", result })
+        res.json({ message: "Ack of this message has been sent to your mail", result });
+        client.close();
     } catch (error) {
         console.log(error);
-        res.json({ message: "Entered mail is not exist.",error });
-    } finally {
-        client.close();
+        res.json({ message: "Entered mail is not exist.", error });
     }
 })
 
@@ -89,13 +88,14 @@ router.get('/message-by-id/:id', async (req, res) => {
     try {
         const client = await mongoClient.connect(DB_URL);
         const db = client.db('secretMessage');
-        const result = await db.collection('secretMessage').find({ _id: objectId(req.params.id) }).project({ password: 0, _id: 0, key: 0 }).toArray();
-        res.status(200).json({ message: "message have been fetched successfully.", result })
+        const result = await db.collection('secretMessage')
+            .find({ _id: objectId(req.params.id) })
+            .project({ password: 0, _id: 0, key: 0 }).toArray();
+        res.status(200).json({ message: "message have been fetched successfully.", result });
+        client.close();
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
-    } finally {
-        client.close();
     }
 })
 
@@ -110,16 +110,15 @@ router.delete('/delete-message', async (req, res) => {
                 await db.collection('secretMessage').findOneAndDelete({ key: req.body.secretKey });
                 res.status(200).json({ message: `${secret.mail}'s - message has been deleted successfully.` });
             } else {
-                res.status(401).json({ message: "incorrect password!" })
+                res.status(401).json({ message: "incorrect password!" });
             }
         } else {
-            res.status(404).json({ message: "secret key not found!!!" })
+            res.status(404).json({ message: "secret key not found!!!" });
         }
+        client.close();
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
-    } finally {
-        client.close()
     }
 })
 
